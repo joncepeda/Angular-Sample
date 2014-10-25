7d7074276991af39ae0063e357210bed2d5a1916
joncepeda <jon.jonathancepeda@gmail.com>
2014-10-26 00:18:31 +0800

    Newest

--- js/app.js
+++ js/app.js
@@ -1,34 +1,27 @@
-<script>
-	var demoApp = angular.module('demoApp', ['ngRoute']);
-	demoApp.config(['$routeProvider', function ($routeProvider){
-		$routeProvider
-			.when('/view1',
-			{
-				controller: 'SimpleController',
-				templateUrl: 'view1.html'
-			})
-			.when('/view2',
-			{
-				controller: 'SimpleController',
-				templateUrl: 'view2.html'
-			})
-			.otherwise({ redirectTo : '/view1'});
-	}]);
-	demoApp.controller('SimpleController',function ($scope){
-		$scope.customers = [
-
-			{name:'Jon',city:'Makati City'},
-			{name:'Jeszza',city:'Makati City'},
-			{name:'Gelo',city:'Caloocan City'},
-			{name:'Shaw',city:'Pasig City'}
-
-		];
-
-		$scope.addCustomer = function(){
-			$scope.customers.push({
-				name:$scope.newCustomer.name, 
-				city: $scope.newCustomer.city 
-			});
-		};
+var demoApp = angular.module('demoApp', ['ngRoute']);
+demoApp.config(['$routeProvider', function ($routeProvider){
+	$routeProvider
+		.when('/view1',
+		{
+			controller: 'SimpleController',
+			templateUrl: 'view1.html'
+		})
+		.when('/view2',
+		{
+			controller: 'SimpleController',
+			templateUrl: 'view2.html'
+		})
+		.otherwise({ redirectTo : '/view1'});
+}]);
+	demoApp.controller('SimpleController',['$scope','http',function ($scope,$http){
+		http.get('js/dataRecords.json').success(function(data){
+			$scope.customers = data;
 	});
-</script>
+
+	$scope.addCustomer = function(){
+		$scope.customers.push({
+			name:$scope.newCustomer.name, 
+			city: $scope.newCustomer.city 
+		});
+	};
+}]);
